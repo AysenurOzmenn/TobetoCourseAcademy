3c975e75030d8619216de45a905b3645a66ca931
Ayşenur Özmen <142101336+AysenurOzmenn@users.noreply.github.com>
2023-11-24 09:52:04 +0300

    added AutoFac

---
+++ Business/ValidationRules/FluentValidation/CourseValidator.cs
@@ -0,0 +1,26 @@
+﻿using Entities.Concretes;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class CourseValidator : AbstractValidator<Course>
+    {
+        public CourseValidator() 
+        {
+            RuleFor(c=>c.Name).NotEmpty();
+            RuleFor(c=>c.Name).MinimumLength(2);
+            RuleFor(c => c.Price).Equals(0);
+            RuleFor(c => c.Name).Must(StartWithA).WithMessage("Kurslar A harfi ile başlamalı");
+        }
+
+        private bool StartWithA(string arg)
+        {
+            return arg.StartsWith("A");
+        }
+    }
+}
